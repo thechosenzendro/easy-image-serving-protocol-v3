398a0c15f345eb43b078d1e9df03ff53fd4935cd
TheChosenZendro <79451876+thechosenzendro@users.noreply.github.com>
2022-09-30 20:09:29 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+
+let data = []
+for ( let i = 0; i < 25; i++) {
+    data.push(Math.floor(Math.random() * 26))
+} 
+
+let x = 0;
+let y = 0;
+for (let i = 0; i <= data.length - 1; i++) {
+    if (x >= 4) {
+        x = 0
+        y += 1
+    }
+    if (data[i] >= 1) {
+        let bright = data[i] * 10
+        led.plotBrightness(x, y, bright)
+    }
+    if (x != 4) {
+        x += 1;
+    }
+}
